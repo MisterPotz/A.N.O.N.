6b07aff37b264e1dcd71fa2c4ec17ad7addf8574
Alexander Gornostaev <aleksgornostaev@gmail.com>
2019-07-29 21:37:32 +0300

    Some modifications in cartographer options, footmap settings changed

--- bot/src/cartographer_config/bot_lds_2d_gazebo.lua
+++ bot/src/cartographer_config/bot_lds_2d_gazebo.lua
@@ -22,14 +22,14 @@ options = {
   tracking_frame = "base_footprint", -- imu_link, If you are using gazebo, use 'base_footprint' (libgazebo_ros_imu's bug)
   published_frame = "odom",
   odom_frame = "odom",
-  provide_odom_frame = false,
-  publish_frame_projected_to_2d = false,
+  provide_odom_frame = false, --false
+  publish_frame_projected_to_2d = true, --false
   use_odometry = true,
   use_nav_sat = false,
   use_landmarks = false,
-  num_laser_scans = 1,
+  num_laser_scans = 1, 
   num_multi_echo_laser_scans = 0,
-  num_subdivisions_per_laser_scan = 1,
+  num_subdivisions_per_laser_scan = 1, -- 1
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.3, -- 0.2
   submap_publish_period_sec = 0.3,
@@ -45,12 +45,15 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.min_range = 0.1
-TRAJECTORY_BUILDER_2D.max_range = 3.5
+TRAJECTORY_BUILDER_2D.max_range = 4
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1 -- no option
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
-
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 1e3 -- less likely to move map linearily
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 1e3 -- less likely to move map angularily
+-- check more options on cartographer site in order to better tune cartographer
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
